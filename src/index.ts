a4ca25cfc9ae9edcced66595321a07391459e0ea
Soareslili <lidianesantossoares2@gmail.com>
2025-05-16 18:35:02 -0300

    Iniciando aula de Typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function primeiroItem<T>(meuArray: T[]): T{
+
+    return meuArray[0]
+}
+
+primeiroItem(['Lidiane', 'Alicia', 'Nalva'])
+primeiroItem([32, 4, 62])
+primeiroItem([{name: 'Lidiane'}])
+
+
